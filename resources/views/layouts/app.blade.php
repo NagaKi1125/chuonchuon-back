826e97f62f21669d0b8433b4815027a6dedad5a9
NagaKi1125 <nqhvu.18it2@sict.udn.vn>
2021-11-17 08:29:09 +0700

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,13 +13,13 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ sercure_asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ sercure_asset('css/admin.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/admin.css') }}">
 
         <!-- Scripts -->
 
-        <script src="{{ sercure_asset('js/app.js') }}" defer></script>
-        <script src="{{ sercure_asset('js/child.js') }}" defer></script>
+        <script src="{{ secure_asset('js/app.js') }}" defer></script>
+        <script src="{{ secure_asset('js/child.js') }}" defer></script>
 
     </head>
     <body class="font-sans antialiased">
