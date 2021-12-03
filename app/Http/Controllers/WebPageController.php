837e019dfb22e084faa1867017dedf88b71ce141
NagaKi1125 <nqhvu.18it2@sict.udn.vn>
2021-12-03 22:19:22 +0700

    deploy frontend

--- app/Http/Controllers/WebPageController.php
+++ app/Http/Controllers/WebPageController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\City;
-use Request;
+use Illuminate\Http\Request;
 use Stevebauman\Location\Facades\Location;
 use Illuminate\Support\Facades\Http;
 use App\Models\Daily;
